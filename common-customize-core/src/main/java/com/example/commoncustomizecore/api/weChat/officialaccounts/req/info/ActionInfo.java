3b010f1474eae6ed130be5c4f9b8bcc97a029799
zhanggaoqiangcxy <18796261167@163.com>
2021-09-02 18:26:09 +0800

    微信公众号二维码参数
    微信公众号二维码参数

--- common-customize-core/src/main/java/com/example/commoncustomizecore/api/weChat/officialaccounts/req/info/ActionInfo.java
+++ common-customize-core/src/main/java/com/example/commoncustomizecore/api/weChat/officialaccounts/req/info/ActionInfo.java
@@ -7,17 +7,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 @Data
 public class ActionInfo
 {
-    /**
-     * 场景值ID，
-     * 临时二维码时为32位非0整型，
-     * 永久二维码时最大值为100000（目前参数只支持1--100000）
-     */
-    private int scene_id;
-
-    /**
-     * 场景值ID（字符串形式的ID），字符串类型，长度限制为1到64
-     */
-    private String scene_str;
+    private QrcodeSceneInfo scene;
 
     @Override
     public String toString()
